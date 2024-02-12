e37a5d675d2b63e4d435fa1a42bfea05af8d25ae
Super User <viktorbutkovich@comcast.net>
2024-02-12 16:47:03 -0600

    Added grid coordinate system plans

--- nle/agent/agent_util/nle_map.py
+++ nle/agent/agent_util/nle_map.py
@@ -14,12 +14,49 @@ class nle_map():
         self.agent = agent
         self.grid: List[List[cell]] = [[cell((0, 0))]]
         self.agent_coordinates: Tuple[int, int] = ((self.agent.x, self.agent.y))
+        self.origin_coordinates = self.agent_coordinates
+        return
+    
+    def create_cell(self, coordinates):
+        '''
+        Grid is initialized with an initial origin cell, like [[.]]
+        Given grid [[.]], to add cell at coordinate (-1, 1) based on an observation:
+
+            Identify that the grid is empty and create a and create <0, 0> cell, leading to grid []
+        '''
+        return
+
+    def get_cell(self, coordinates):
+        '''
+        Room example:
+            !--
+            -.-
+            ---
+            Center has coordinates (0, 0), top left has coordinates (-1, 1), and bottom right has coordinates (1, -1)
+            These should be stored as cells with glyphs:
+                [
+                    [!, -, -],
+                    [-, ., -],
+                    [-, -, -]
+                ]
+            This means that, with (0, 0) being in the center, the map would remember the origin row, column with <0, 0> corresponding to coordinates (-1, 1):
+                Get cell at coordinates (0, 0): return grid[1][1]
+                    Y coordinate 0 is equal to row 1, since it is 1 less than the origin row's Y coordinate
+                    X coordinate 0 is equal to column 1, since it is 1 more than the origin column's X coordinates
+                Get cell at coordinates (-1, 1): return grid[0][0]
+                    Y coordinate 1 is equal to row 0, since it is equal to the origin row's Y coordinate
+                    X coordinate -1 is equal to column 0, since it is equal to the origin row's X coordinate
+                Get cell at coordinate (1, 0): return grid [1][2]
+                    Y coordinate 0 is equal to row 1, since it is 1 less than the origin row's Y coordinate
+                    X coordinate 1 is equal to column 2, since it is 2 more than the origin column's X coordinate
+            To gain this behavior, get_cell((x, y)) should return self.grid[self.origin_y - y][x - self.origin_x]
+                get_cell((1, 0)) with self.origin = (-1, 1) returns self.grid[1 - 0][1 - (-1)] = self.grid[1][2] - intended behavior
+        '''
         return
 
     def update_surroundings(self, text_glyphs: List[str]):
-        print(text_glyphs)
-        print('Update encoded map with the above text glyphs ^')
-        return
+        for text_glyph in text_glyphs:
+            return
     
     def __str__(self):
         return_str = ""
