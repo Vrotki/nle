d0f79193949a9756446ba12565e0d923d5ed8704
Super User <viktorbutkovich@comcast.net>
2024-02-13 13:38:11 -0600

    Implemented create_cell logic

--- nle/agent/agent_util/nle_map.py
+++ nle/agent/agent_util/nle_map.py
@@ -1,10 +1,10 @@
 from typing import List, Tuple
 
 class cell():
-    def __init__(self, coordinates: Tuple[int, int]) -> None:
+    def __init__(self, coordinates: Tuple[int, int], glyph: str = ".") -> None:
         self.x: int = coordinates[0]
         self.y: int = coordinates[1]
-        self.glyph: str = "."
+        self.glyph: str = glyph
     
     def __str__(self) -> str:
         return(self.glyph)
@@ -19,68 +19,40 @@ class nle_map():
         self.origin_coordinates = self.agent_coordinates
     
     def create_cell(self, coordinates: Tuple[int, int]) -> None:
-        '''
-        Grid is initialized with an initial origin cell, like [[.]]
-        Given grid [[.]], to add cell at coordinate (-1, 1) based on an observation:
-        
-        '''
-        row, col = self.to_row_col(coordinates)
         while coordinates[1] > self.origin_coordinates[1]: # If new cell would be above current origin, need to move origin up and add new rows
-            # Increase grid height by 1
-            return # Add rows to top of map until new cell is accomodated, moving origin 1 up each time
+            self.grid_height += 1
+            self.origin_coordinates = (self.origin_coordinates[0], self.origin_coordinates[1] + 1)
+            self.grid.insert(0, [cell(self.to_coordinates((0, col_index))) for col_index in range(self.grid_width)])
+            # Add rows to top of map until new cell is accomodated, moving origin 1 up each time
+
         while coordinates[0] < self.origin_coordinates[0]: # If new cell would be to left of current origin, need to move origin left and add new columns
-            # Increase grid width by 1
-            return # Add columns to left of map until new cell is accomodated, moving origin 1 left each time
+            self.grid_width += 1
+            self.origin_coordinates = (self.origin_coordinates[0] - 1, self.origin_coordinates[1])
+            col_index = 0
+            for row_index, row in enumerate(self.grid):
+                row.insert(0, cell(self.to_coordinates((row_index, col_index))))
+            # Add columns to left of map until new cell is accomodated, moving origin 1 left each time
 
-        # Need to add rows to bottom if difference between coordinates[1] and origin[1] is greater than length of grid?
-        # While origin_y + grid_height <= new_y_coordinate, add rows to bottom
-        #   To add y coordinate -2 when origin is (0, 0) and grid height is 1,
-        #       origin_y + grid_height = 1, which is <= 2, so add a row to bototm
-        #       Origin is still (0, 0), but grid height is 2
-        #       origin_y + grid_height = 2, which is <= 2, so add a row to bottom
-        #       Origin is still (0, 0, but grid height is 3
-        #       origin_y + grid_height = 3, which is not <= 2, so done adding rows
-        while self.origin_coordinates[1] + self.grid_height <= coordinates[1]: # While distance from origin to bottom of grid shows that new cell doesn't have a row yet
-            # Increase grid height by 1
-            return # Add rows to bottom of map until new cell is accomodated
+        while self.origin_coordinates[1] - coordinates[1] >= self.grid_height: # While distance from origin to bottom of grid shows that new cell doesn't have a row yet
+            self.grid_height += 1
+            self.grid.append([cell(self.to_coordinates((self.grid_height - 1, col_index))) for col_index in range(self.grid_width)])
         
-        # Continue process for adding new columns
-
-        return
+        while self.origin_coordinates[0] + self.grid_width <= coordinates[0]: # While distance from origin to right of grid shows that new cell doesn't have a column yet
+            self.grid_width += 1
+            col_index = self.grid_width - 1
+            for row_index, row in enumerate(self.grid):
+                row.append(cell(self.to_coordinates((row_index, col_index))))
 
     def to_row_col(self, coordinates: Tuple[int, int]) -> Tuple[int, int]:
         return((self.origin_coordinates[1] - coordinates[1], coordinates[0] - self.origin_coordinates[0]))
 
+    def to_coordinates(self, row_col: Tuple[int, int]) -> Tuple[int, int]:
+        return((row_col[1] + self.origin_coordinates[0], self.origin_coordinates[1] - row_col[0]))
+
     def get_cell(self, coordinates: Tuple[int, int]) -> cell:
-        '''
-        Room example:
-            !--
-            -.-
-            ---
-            Center has coordinates (0, 0), top left has coordinates (-1, 1), and bottom right has coordinates (1, -1)
-            These should be stored as cells with glyphs:
-                [
-                    [!, -, -],
-                    [-, ., -],
-                    [-, -, -]
-                ]
-            This means that, with (0, 0) being in the center, the map would remember the origin row, column with <0, 0> corresponding to coordinates (-1, 1):
-                Get cell at coordinates (0, 0): return grid[1][1]
-                    Y coordinate 0 is equal to row 1, since it is 1 less than the origin row's Y coordinate
-                    X coordinate 0 is equal to column 1, since it is 1 more than the origin column's X coordinates
-                Get cell at coordinates (-1, 1): return grid[0][0]
-                    Y coordinate 1 is equal to row 0, since it is equal to the origin row's Y coordinate
-                    X coordinate -1 is equal to column 0, since it is equal to the origin row's X coordinate
-                Get cell at coordinate (1, 0): return grid [1][2]
-                    Y coordinate 0 is equal to row 1, since it is 1 less than the origin row's Y coordinate
-                    X coordinate 1 is equal to column 2, since it is 2 more than the origin column's X coordinate
-            To gain this behavior, get_cell((x, y)) should return self.grid[self.origin_y - y][x - self.origin_x]
-                get_cell((1, 0)) with self.origin = (-1, 1) returns self.grid[1 - 0][1 - (-1)] = self.grid[1][2] - intended behavior
-        '''
         row, col = self.to_row_col(coordinates)
         try:
             return(self.grid[row][col])
-            #return(self.grid[self.origin_coordinates[1] - coordinates[1]][coordinates[0] - self.origin_coordinates[0]])
         except:
             return(None)
 
@@ -95,6 +67,9 @@ class nle_map():
             for column in row:
                 return_str += str(column)
                 if column != row[-1]:
-                    return_str += ', '
-                return_str += ']\n'
+                    return_str += ' '
+            return_str += ']'
+            if row != self.grid[-1]:
+                return_str += ', '
+            return_str += '\n'
         return(return_str)
